3f04113b0220f53fec9db6e5ccb64d34f6cbacbd
Joseph Mckenzi <josephmckenzieaz@yahoo.com>
2019-06-07 19:24:12 -0400

    Hello world, and send text using twilio,envs using godotenv

---
+++ src/send-text/text.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fromNumber := os.Getenv("fromNumber")
+	accountSid := os.Getenv("accountSid")
+	authToken := os.Getenv("authToken")
+
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+
+	msgData := url.Values{}
+	msgData.Set("To", "+17244912584")
+	msgData.Set("From", fromNumber)
+	msgData.Set("Body", "Using an Env")
+	msgDataReader := *strings.NewReader(msgData.Encode())
+
+	// Create HTTP request client
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+	req.SetBasicAuth(accountSid, authToken)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Make HTTP POST request and return message SID
+	resp, _ := client.Do(req)
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		var data map[string]interface{}
+		decoder := json.NewDecoder(resp.Body)
+		err := decoder.Decode(&data)
+		if err == nil {
+			fmt.Println(data["sid"])
+		}
+	} else {
+		fmt.Println(resp.Status)
+	}
+}
